35729eeea6c76a3e43fd5fc36a84f138f636a04e
Jhansi Lakshmi <djhansi@altimetrik.com>
2025-04-08 21:50:31 +0530

    Commit 1

--- Test/Controllers/HomeController.cs
+++ Test/Controllers/HomeController.cs
@@ -26,6 +26,7 @@ namespace Test.Controllers
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
+            string commitno = "Commit 1";
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
