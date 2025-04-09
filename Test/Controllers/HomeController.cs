f385ae0ce8cd2883c964ecf7eb24a99e18f80aa6
Jhansi Lakshmi <djhansi@altimetrik.com>
2025-04-09 07:03:25 +0530

    merge comit

--- Test/Controllers/HomeController.cs
+++ Test/Controllers/HomeController.cs
@@ -26,7 +26,7 @@ namespace Test.Controllers
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
-            string commitno = "Commit 3";
+            string commitno = "Commit 3 merge";
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
